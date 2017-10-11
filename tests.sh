d3889d5e8d1044832451a4e802bf506a5787f37a
Lauri Korts-Pärn <lauri@korc.jp>
2017-10-12 04:21:56 +0800

    improve blockdev_uuid test

--- tests.sh
+++ tests.sh
@@ -9,7 +9,7 @@
 : ${kernel:=/boot/vmlinuz-$kver}
 : ${initrd:=ramdisk-$kver}
 : ${initrd_net:=ramdisk_net-$kver}
-: ${kvm_cmd:=kvm -m 4096 -vga qxl}
+: ${kvm_cmd:=kvm -m 4096 -vga qxl -snapshot}
 : ${init:=/bin/systemd}
 : ${test:=test_${1:-smb_direct}}
 : ${dev:=/dev/sdb}
@@ -17,6 +17,10 @@
 test -n "$srv_test" || {
   if test "x$srv" = "x10.0.2.2";then srv_test="127.0.0.1"
   else srv_test="$srv";fi
+}
+
+ERROR() {
+  exit 1
 }
 
 test_http_copyup() {
@@ -36,9 +40,9 @@ test_smb_direct() {
 }
 
 test_blockdev_uuid() {
-  test -r "$dev" || { echo "Need to be able to read \$dev ($dev)" >&2 ; return 1; }
-  test -n "$uuid" || { echo "Need to define \$uuid ($uuid)" >&2 ; return 1; }
-  $kvm_cmd -drive file="$dev",if=virtio,readonly -kernel "$kernel" -initrd "$initrd" -append "root=UUID=$uuid:$share/*.sfs+:$share/\$arch/*kernel-\$kver.sfs+mem $append"
+  test -r "$dev" || ERROR "Cannot read '$dev'. Run: sudo setfacl -m u:$USER:r $dev"
+  test -n "$uuid" || ERROR "Need UUID. export uuid=xxxxx before run."
+  $kvm_cmd -drive file="$dev",if=virtio -kernel "$kernel" -initrd "$initrd" -append "root=UUID=$uuid:$share/*.sfs+:$share/\$arch/*kernel-\$kver.sfs+mem $append"
 }
 
 test_blockdev_uuid_mem() {
